3fc0210ea96921f3147436bc809fac94c9aa148d
houtaogo <houtaogo@163.com>
2022-03-30 13:18:13 +0800

    hot-fix commit

--- src/main/java/com/ht/git/GitTest.java
+++ src/main/java/com/ht/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("git test");
         System.out.println("git test002");
         System.out.println("git test003");
+        System.out.println("git test004");
     }
 }
